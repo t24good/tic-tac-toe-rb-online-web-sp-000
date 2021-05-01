fc8814b39599e94beb12e20ec4b112b3d5fd45ab
Tanesha Goodman <t24good@icloud.com>
2021-05-01 19:30:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,7 +43,7 @@ def turn(board)
 
   if valid_move?(board, index)
     token = current_player(board)
-    move(board, index, token)
+    move(board, index, current_player(board))
     display_board(board)
   else
     puts "Whoops! That is not a valid move"
