2f167e1ee721a06e11c305752172471e44a36d51
Tanesha Goodman <t24good@icloud.com>
2021-05-01 19:44:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -71,3 +71,9 @@ def won?(board)
     position_1 == "O" && position_2 == "O" && position_3 == "O"
   end
 end
+
+def full?(board)
+  board.all? do |position|
+    position == "X" || position == "O"
+  end
+end
