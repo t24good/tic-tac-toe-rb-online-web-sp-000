d7f288de38bb3306dae62308dc41a945789a77e5
Tanesha Goodman <t24good@icloud.com>
2021-05-01 19:44:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -67,7 +67,7 @@ def won?(board)
     position_2 = board[win_combo[1]]
     position_3 = board[win_combo[2]]
 
-    position_1 == "X" && position_2 == "X" position_3 == "X"
-    position_1 == "O" && position_2 == "O" position_3 == "O"
+    position_1 == "X" && position_2 == "X" && position_3 == "X"
+    position_1 == "O" && position_2 == "O" && position_3 == "O"
   end
 end
