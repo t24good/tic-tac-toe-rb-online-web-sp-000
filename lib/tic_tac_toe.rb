8369256cf48f40aca11f1a181b316cd59c76983b
Tanesha Goodman <t24good@icloud.com>
2021-05-01 19:00:12 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,8 +40,8 @@ def turn(board)
   puts "Please enter number between 1-9"
   input = gets.strip
   index = input_to_index(input)
-  
-end  
+
+end
 
 def turn_count(board)
   board.count do |position|
