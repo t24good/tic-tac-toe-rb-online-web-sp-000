69c48d402e273eae03c5aa46836537ffef4471af
Tanesha Goodman <t24good@icloud.com>
2021-05-01 18:38:30 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,3 +26,12 @@ end
 def move(board, index, token)
   board[index] = token
 end
+
+
+def position_taken?(board, index)
+  board[index] == "X" || board[index] == "O"
+end
+
+def valid_move?(board, index)
+  index.between?(0,8)
+end
